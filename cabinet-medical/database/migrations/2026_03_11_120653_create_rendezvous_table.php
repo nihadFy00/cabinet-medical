89e1b618522aedc12c8a9752aacc7bb2dc9165e8
laila <lailaouajbir85@gmail.com>
2026-03-11 15:57:03 +0000

    Base de données complète et fonctionnelle avec relations

---
+++ cabinet-medical/database/migrations/2026_03_11_120653_create_rendezvous_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('rendezvous', function (Blueprint $table) {
+        $table->id();
+        $table->dateTime('date_rdv');
+        $table->enum('statut', ['en_attente', 'confirme', 'annule', 'termine'])->default('en_attente'); // Gestion des statuts [cite: 88]
+        $table->text('motif')->nullable();
+        $table->foreignId('patient_id')->constrained()->onDelete('cascade');
+        $table->foreignId('medecin_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rendezvous');
+    }
+};
