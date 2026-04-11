a6ae337e25fac269a47262a15e64c1b6b1472d70
nacira <naciraaitfaraji2023@gmail.com>
2026-04-11 13:55:53 +0100

    suppression middleware auth webtest.php

--- module_rendez_vous/webtest.php
+++ module_rendez_vous/webtest.php
@@ -2,10 +2,6 @@
 use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('rendezvous', AppointmentController::class);
-    Route::patch('rendezvous/{rendezvous}/confirm', [AppointmentController::class, 'confirm'])
-        ->name('rendezvous.confirm');
-    Route::post('rendezvous/{rendezvous}/reminder', [AppointmentController::class, 'sendReminder'])
-        ->name('rendezvous.reminder');
-});
+Route::resource('rendezvous', AppointmentController::class);
+Route::patch('rendezvous/{rendezvous}/confirm', [AppointmentController::class, 'confirm'])->name('rendezvous.confirm');
+Route::post('rendezvous/{rendezvous}/reminder', [AppointmentController::class, 'sendReminder'])->name('rendezvous.reminder');
